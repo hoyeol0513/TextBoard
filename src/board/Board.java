fb5b0bc45d5a36debe8a7cef9ff71e6a75f7c51d
hoyeol0513@naver.com <hoyeol0513@naver.com>
2022-04-12 19:42:15 +0900

    TextBoard Update & Delete Method

--- src/board/Board.java
+++ src/board/Board.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Board {
+
+	ArrayList<String> titles = new ArrayList<>();
+	ArrayList<String> bodies = new ArrayList<>();
+	ArrayList<Integer> numbers = new ArrayList<>(); //게시물 번호 저장소
+	
+	int no = 1; //게시물 번호
 	
 	public void runBoard() {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		ArrayList<String> titles = new ArrayList<>();
-		ArrayList<String> bodies = new ArrayList<>();
-		ArrayList<Integer> numbers = new ArrayList<>(); //게시물 번호 저장소
-		int no = 1; //게시물 번호
 		
 		while(true) {
-			System.out.println("명령어를 입력해주세요 : ");
+			System.out.print("명령어를 입력해주세요 : ");
 			String cmd = sc.nextLine();
 			
 			if(cmd.equals("add")) {
@@ -32,30 +34,16 @@ public class Board {
 				titles.add(title);
 				bodies.add(body);
 				
-				System.out.print("게시물이 저장되었습니다.");
+				System.out.println("게시물이 저장되었습니다.");
 				no++; //게시물 번호 자동 증가
 			} else if(cmd.equals("list")){
-				for(int i =0; i<titles.size();i++) {
-					int ano = numbers.get(i);
-					String title = titles.get(i);
-					String body = bodies.get(i);
-					
-					System.out.println("번호 : " + ano);
-					System.out.println("제목 : " + title);
-					System.out.println("====================================");				
-					}
+				ShowList();
 			} else if (cmd.equals("update")) {
 				System.out.print("수정할 게시물 번호 : ");
 				int targetNo = Integer.parseInt(sc.nextLine()); //게시물 번호
-				int targetIndex = -1;//게시물번호 저장소 인덱스
-				//0으로 지정하면 ArrayList 인덱스에 포함되기 때문에 안됨. -1이 적합.
-				for(int i = 0; i<numbers.size(); i++) {
-					int currentNo = numbers.get(i);
-					if(targetNo == currentNo) {
-						targetIndex = i; //i의 값이 중요함. 이걸 찾으면 해당 내용, 제목을 알 수 있음.
-						break; //i의 값을 았기 때문에 종료.
-					}
-				}
+				
+				int targetIndex = getIndexofArticleNo(targetNo);
+				//게시물 번호를 발견했을 경우엔 targetIndex를 i로 지정
 				
 				if(targetIndex == -1) { //게시물 번호를 발견하지 못했을 경우엔 -1 그대로이기 때문에
 					System.out.println("없는 게시물입니다.");
@@ -68,9 +56,53 @@ public class Board {
 					titles.set(targetIndex,title); //게시물 제목 수정
 					bodies.set(targetIndex,body); //게시물 내용 수정		
 					System.out.println("수정이 완료되었습니다.");
+					ShowList();
+					
 				}
+			}else if(cmd.equals("delete")) {
+				System.out.print("삭제할 게시물 번호 : ");
+				int targetNo = Integer.parseInt(sc.nextLine());
 				
+				int targetIndex = getIndexofArticleNo(targetNo);
+				
+				if(targetIndex == -1) {
+					System.out.println("없는 게시물입니다.");
+				}else {
+					titles.remove(targetIndex);
+					bodies.remove(targetIndex);
+					numbers.remove(targetIndex); //번호도 삭제해야 함.
+					
+					System.out.println("삭제가 완료되었습니다.");
+					no--;
+					ShowList();
+				}
 			}
+		}
+	}
+	public int getIndexofArticleNo(int targetNo) {
+		int targetIndex = -1;//게시물번호 저장소 인덱스
+		//0으로 지정하면 ArrayList 인덱스에 포함되기 때문에 안됨. -1이 적합.
+		for(int i = 0; i<numbers.size(); i++) {
+			int currentNo = numbers.get(i);
+			if(targetNo == currentNo) {
+//				targetIndex = i; //i의 값이 중요함. 이걸 찾으면 해당 내용, 제목을 알 수 있음.
+//				break; //i의 값을 았기 때문에 종료.
+				return i;
+			}
+		}
+		//return targetIndex;
+		return -1; //for문을 다 돌았을 때까지 없으면 targetIndex를 -1로 반환
+	}
+	
+	public void ShowList() {
+		for(int i =0; i<titles.size();i++) {
+			int ano = numbers.get(i);
+			String title = titles.get(i);
+			String body = bodies.get(i);
+			
+			System.out.println("번호 : " + ano);
+			System.out.println("제목 : " + title);
+			System.out.println("=====================");									
 		}
 	}
 }
